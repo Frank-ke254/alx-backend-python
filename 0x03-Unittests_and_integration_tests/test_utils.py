8c38ef9dc34d2550c1e7a1a207c80f80891bd315
Francis <kahotadfrajeh@gmail.com>
2025-09-19 11:33:25 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -72,6 +72,28 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+            
+class TestMemoize(unittest.TestCase):
+    """
+    TestMemoize class contains unit tests for the memoize decorator which enables us verify
+    that a method is only executed once.
+    """
+    def test_memoize(self) -> None:
+        """Test that memoize method is only called once."""
+        class TestClass:
+            def a_method(self) -> int:
+                return 42
+
+            @memoize
+            def a_property(self) -> int:
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+            self.assertEqual(obj.a_property, 42)
+            self.assertEqual(obj.a_property, 42)
+            mock_method.assert_called_once()
+
 
 
 if __name__ == "__main__":
