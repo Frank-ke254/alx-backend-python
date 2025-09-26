9e81a2a16db218b82f04c6d34bd8cd2d4a1f088b
Frank-ke254 <kahotadfrajeh@gmail.com>
2025-09-26 11:09:43 +0300

    Revised files with errors rectified

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.MessagePagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
 }
 
