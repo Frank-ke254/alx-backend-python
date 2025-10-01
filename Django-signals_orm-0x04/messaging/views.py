e0d3327596edb056031728d96ff87783499f8952
Francis <kahotadfrajeh@gmail.com>
2025-10-01 15:11:10 +0300

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.contrib.auth import logout
+from django.shortcuts import render, get_object_or_404
+from .models import Message
+
+def conversation_view(request, message_id):
+    root_message = (
+        Message.objects.select_related("sender", "receiver")
+        .prefetch_related("replies__sender", "replies__receiver")
+        .get(id=message_id)
+    )
+    thread = get_threaded_messages(root_message)
+    return render(request, "messaging/conversation.html", {"thread": thread})
 
 @login_required
 def delete_user(request):
