974c53533feea17d08dbb312a292a4326747d41b
Francis <kahotadfrajeh@gmail.com>
2025-10-01 15:27:26 +0300

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .managers import UnreadMessagesManager
+
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name="sent_messages", on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name="received_messages", on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
     edited_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         null=True,
